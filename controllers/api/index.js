4f4d9072101ac6fa1aac4469f59bc3260307e859
AMIrene <f.adammanik@gmail.com>
2021-09-28 14:24:19 +1000

    added sequelize connection to index js route file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection'); 
 
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
